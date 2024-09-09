95cb434b48b3b2255ff34b554386d57093a75469
jjuchan <yjc7241@naver.com>
2024-09-09 23:57:21 +0900

    개인 공부

--- src/array/ex/ArrayEx8.java
+++ src/array/ex/ArrayEx8.java
@@ -2,20 +2,22 @@ package array.ex;
 
 import java.util.Scanner;
 
-public class ArrayEx7 {
+public class ArrayEx8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] scores = new int[4][3];
+        System.out.print("학생수를 입력하세요:");
+        int stuedent = sc.nextInt();
+        int[][] scores = new int[stuedent][3];
         String[] subject = {"국어", "영어", "수학"};
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < stuedent; i++) {
             System.out.println((i + 1) + "번 학생의 성적을 입력하세요:");
             for (int j = 0; j < 3; j++) {
                 System.out.print(subject[j] + "점수");
                 scores[i][j] = sc.nextInt();
             }
         }
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < stuedent; i++) {
             int total = 0;
             for (int j = 0; j < 3; j++) {
                 total += scores[i][j];
